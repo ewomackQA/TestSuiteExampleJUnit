a6e2c475e2721e99a8dc5c9de178b175a9755210
Womack <EWomack@corp.group.local>
2017-03-29 14:49:02 +0100

    first commit

---
+++ firstTestClass.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class firstTestClass {
+	@Test
+	public void test1() {
+		assertEquals(1, 1);
+	}
+}
---
+++ secondTestClass.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class secondTestClass {
+	@Test
+	public void test1() {
+		assertEquals(1, 2);
+	}
+}
